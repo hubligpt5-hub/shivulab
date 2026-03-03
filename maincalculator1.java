88840f1d8aadf915f07d90098a0b0b1260171cb8
hubligpt5-hub <hubligpt5@gmail.com>
2026-03-03 10:44:28 +0530

    Add files via upload

---
+++ maincalculator1.java
@@ -0,0 +1,29 @@
+class calculator
+{
+  public int calculate(int n1, int n2, char op)
+  {
+    switch(op)
+    {
+      case'+':return n1+n2;
+      case'-':return n1-n2;
+      case'*':return n1*n3;
+      case'/':return 0;
+    }
+  }
+}
+class printresult
+{
+  public void printrs(int res)
+  {
+    System.out.println("the result is:"+res);
+  }
+}
+class maincalculator1
+{ 
+  public static void main(String a[])
+  {
+    calculator=new calculator();
+    n.setdata regno(2);
+    System.out.println(" the regno is:"+n.getdataregno());
+  }
+}
